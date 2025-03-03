4f6876419965a18c1e3619ce79c6342ae335505c
postwo <codo7717@naver.com>
2025-03-04 07:46:43 +0900

    로그인 구현

--- jwt/src/main/java/com/example/jwt/repository/UserRepository.java
+++ jwt/src/main/java/com/example/jwt/repository/UserRepository.java
@@ -7,4 +7,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     //exists는 특정 조건을 만족하는 데이터가 존재하는지 확인할 때 사용
     Boolean existsByUsername(String username);
+
+    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    UserEntity findByUsername(String username);
 }
