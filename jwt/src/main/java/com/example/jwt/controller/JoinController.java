f9097ca83370aa542573421bf8969afe3732ec6d
postwo <codo7717@naver.com>
2025-03-04 07:22:00 +0900

    회원가입 구현

---
+++ jwt/src/main/java/com/example/jwt/controller/JoinController.java
@@ -0,0 +1,23 @@
+package com.example.jwt.controller;
+
+import com.example.jwt.dto.JoinDTO;
+import com.example.jwt.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
--- jwt/src/main/java/com/example/jwt/repository/UserRepository.java
+++ jwt/src/main/java/com/example/jwt/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+    //exists는 특정 조건을 만족하는 데이터가 존재하는지 확인할 때 사용
+    Boolean existsByUsername(String username);
 }
