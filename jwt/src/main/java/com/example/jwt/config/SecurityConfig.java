8bdd988db043d26e238d76d07ed4a739def8e7ed
postwo <codo7717@naver.com>
2025-03-04 06:55:54 +0900

    securityconfig 기본설정

---
+++ jwt/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable = 이렇게 하는이유는 세션방식에서는 세션이 항상 고정되기 때문에 csrf를 필수적으로 방어해야하지만
+        // jwt에서는 세션을 STATELESS 상태로 관리하가 때문에 csrf에 대한 공격을 그렇게 방어할 필요없다 하지만 자신이 막고 싶으면 막아도 상관없다
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
