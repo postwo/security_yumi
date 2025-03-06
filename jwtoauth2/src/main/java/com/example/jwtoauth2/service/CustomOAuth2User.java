04dca9f516403194b7fa8215c571ff98943ed778
postwo <codo7717@naver.com>
2025-03-06 14:33:48 +0900

    oauth 서비스 관련 설정 구현

---
+++ jwtoauth2/src/main/java/com/example/jwtoauth2/service/CustomOAuth2User.java
@@ -0,0 +1,53 @@
+package com.example.jwtoauth2.service;
+
+import com.example.jwtoauth2.dto.UserDTO;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User { // 여기에 같이 userdetails 를 구현 하면 더 깔끔하다
+
+    private final UserDTO userDTO;
+
+    public CustomOAuth2User(UserDTO userDTO) {
+
+        this.userDTO = userDTO;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+
+                return userDTO.getRole();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+
+        return userDTO.getName();
+    }
+
+    public String getUsername() {
+
+        return userDTO.getUsername();
+    }
+}
